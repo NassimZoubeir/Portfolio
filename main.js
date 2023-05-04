8c73638be48e020fc5243d23b0f63eabef5f851b
NassimZoubeir <nassimz@outlook.fr>
2023-05-04 16:28:19 +0200

    ajout de l'api météo

--- main.js
+++ main.js
@@ -15,3 +15,23 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 }
+
+// ------------------------------- API ----------------------------------
+const apiKey = "32fa87f340afa8257e73875d024490e8";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}`;
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    // Utiliser les données pour afficher la météo de Paris
+  })
+  .catch(error => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
